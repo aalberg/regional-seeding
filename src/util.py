cee8af081d8b024fce06a0050d48b99c0adf23d3
aalberg <aralberg@gmail.com>
2016-01-11 22:58:26 -0800

    Config file related fixes
    Config and player file lines are now treated as comments if the first
    non-whitespace character of the line is '#'

--- src/util.py
+++ src/util.py
@@ -8,7 +8,7 @@ class PoolType:
 
 # Load functions.
 def ParseConfigLine(line, config_types):
-  split_line = line.split(' ', 1)
+  split_line = line.split()
   for i in xrange(len(split_line)):
     split_line[i] = split_line[i].strip()
   split_line = filter(None, split_line)
@@ -17,17 +17,15 @@ def ParseConfigLine(line, config_types):
 
   field = split_line[0].strip()
   value = split_line[1].strip()
-  try:
-    if field not in config_types or config_types[field] == 'string':
-      return (field, value)
-    elif config_types[field] == 'int':
-      return (field, int(value))
-    elif config_types[field] == 'float':
-      return (field, float(value))
-    elif config_types[field] == 'bool':
-      return (field, bool(value))
-  except ValueError:
-    print "Could not parse config line."
+  if field not in config_types or config_types[field] == 'string':
+    return (field, value)
+  elif config_types[field] == 'int':
+    return (field, int(value))
+  elif config_types[field] == 'float':
+    return (field, float(value))
+  elif config_types[field] == 'bool':
+    return (field, bool(value))
+  else:
     return (None, None)
 
 def LoadConfig(config, config_types):
@@ -35,14 +33,16 @@ def LoadConfig(config, config_types):
     with open(config["config_file"]) as f:
       line_num = 1
       for line in f:
-        if line.lstrip()[0] == '#':
-          continue
-        field, value = ParseConfigLine(line, config_types)
-        if not field == None:
-          print "Found config setting: " + field + ": \"" + str(value) + "\" " + str(type(value))
-          config[field] = value
-        else:
-          print "Skipping config line:", line_num
+        if not IsCommentLine(line):
+          try:
+            field, value = ParseConfigLine(line, config_types)
+            if not field == None and not value == None:
+              print "Found config setting: " + field + ": \"" + str(value) + "\" " + str(type(value))
+              config[field] = value
+            else:
+              print "Skipping config line:", line_num
+          except ValueError:
+            print "Could not parse config line:", line_num
         line_num += 1
     print "Loaded config settings"
     print "-"*60
@@ -54,26 +54,30 @@ def LoadPlayers(config, player_map):
     with open(config["player_in_file"]) as f:
       line_num = 1
       for line in f:
-        split_line = line.split(",")
-        for i in xrange(len(split_line)):
-          split_line[i] = split_line[i].strip()
-        split_line = filter(None, split_line)
+        if not IsCommentLine(line):
+          split_line = line.split(",")
+          for i in xrange(len(split_line)):
+            split_line[i] = split_line[i].strip()
+          split_line = filter(None, split_line)
 
-        if len(split_line) == 3:
-          try:
-            player_map.AddPlayerRaw(split_line[0], split_line[1], split_line[2])
-            print "Found player: " + split_line[0] + ", " + split_line[1] + ", " + split_line[2]
-          except ValueError:
-            print "Count not parse player line:", line_num
-        else:
-          print "Skipping player line:", line_num
-          print split_line
+          if len(split_line) == 3:
+            try:
+              player_map.AddPlayerRaw(split_line[0], split_line[1], split_line[2])
+              print "Found player: " + split_line[0] + ", " + split_line[1] + ", " + split_line[2]
+            except ValueError:
+              print "Count not parse player line:", line_num
+          else:
+            print "Skipping player line:", line_num
+            print split_line
         line_num += 1
     print "Loaded player data"
   except IOError:
     print "No player data file found. Exiting."
     sys.exit(0)
 
+def IsCommentLine(line):
+  return len(line.lstrip()) > 0 and line.lstrip()[0] == '#'
+    
 # Output functions.
 def PrintStr(param):
   print param[0]
